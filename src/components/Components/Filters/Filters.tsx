62eb3b738600b486dc5dcc05181e2ae8cda80989
szymon.morawski <szymon.morawski@paradoxinteractive.com>
2021-07-25 19:59:21 +0200

    Add new icon for PoK filter

--- src/components/Components/Filters/Filters.tsx
+++ src/components/Components/Filters/Filters.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, useEffect } from 'react';
+import PoKIcon from '../../Components/Icons/PoK';
 import { ObjectiveType, GameExpansion } from '../../../resources/types';
 
 import styles from './Filters.module.scss';
@@ -60,6 +61,11 @@ const Filters = ({ onChange }: FiltersProps) => {
     [filter, onChange]
   );
 
+  const isPoKActive = !filter.expansions || filter.expansions.length === 0;
+  const isSecretActive = !!filter.types?.includes(ObjectiveType.Secret);
+  const isPublic1Active = !!filter.types?.includes(ObjectiveType.Public1);
+  const isPublic2Active = !!filter.types?.includes(ObjectiveType.Public2);
+
   return (
     <div className={styles.container}>
       <input
@@ -73,28 +79,31 @@ const Filters = ({ onChange }: FiltersProps) => {
         className={`${styles.objective} ${styles.public1}`}
         onChange={setTypes}
         value={ObjectiveType.Public1.toString()}
-        checked={!!filter.types?.includes(ObjectiveType.Public1)}
+        checked={isPublic1Active}
       />
       <input
         type="checkbox"
         className={`${styles.objective} ${styles.public2}`}
         onChange={setTypes}
         value={ObjectiveType.Public2.toString()}
-        checked={!!filter.types?.includes(ObjectiveType.Public2)}
+        checked={isPublic2Active}
       />
       <input
         type="checkbox"
         className={`${styles.objective} ${styles.secret}`}
         onChange={setTypes}
         value={ObjectiveType.Secret.toString()}
-        checked={!!filter.types?.includes(ObjectiveType.Secret)}
+        checked={isSecretActive}
       />
-      <input
-        type="checkbox"
-        className={styles.pokExpansion}
-        onChange={includePoK}
-        checked={!filter.expansions || filter.expansions.length === 0 }
-      />
+      <label className={styles.pokExpansion}>
+        <PoKIcon width={18} height={18} color={(isPoKActive ? '#000' : '#cfd0d1')}/>
+        <input
+          type="checkbox"
+          className={styles.pokExpansion}
+          onChange={includePoK}
+          checked={isPoKActive}
+        />
+      </label>
       <button className={styles.clear} onClick={() => setFilter({})}>
         Clear
       </button>
---
+++ src/components/Components/Icons/PoK.tsx
@@ -0,0 +1,36 @@
+interface PoKIcon {
+  width?: number;
+  height?: number;
+  color?: string;
+};
+
+const PoKIcon = ({ width = 25, height = 25, color = "#000" }: PoKIcon) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 29.104165 29.104165"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg">
+
+      <g transform="translate(-22.686997,-1.6039987)">
+        <path
+          fill={color}
+          stroke={color}
+          strokeWidth="0.264583px"
+          d="m 47.625001,19.579166 c -3.974006,3.525108 -10.583333,6.614584 -10.583334,10.583332 1e-6,-3.968748 -6.116956,-7.155243 -10.583333,-10.583332 l 5.291667,-15.8749993 1.322917,15.8749993 3.96875,2.645834 3.96875,-2.645834 1.322916,-15.8749993 z"
+        />
+        <g transform="translate(0,1.322917)">
+          <path
+            fill={color}
+            stroke={color}
+            strokeWidth="0.264583px"
+            d="M 37.041668,18.520833 39.687501,15.875 v -5.291667 0 L 37.041668,2.6458336 34.395834,10.583333 V 15.875 Z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default PoKIcon;
